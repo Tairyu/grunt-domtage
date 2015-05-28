08fcc38aa8241bf7b217a8cf49852393773af052
清水　泰龍 <ta_shimizu@waku-2.com>
2015-05-28 15:03:22 +0900

    added: use recipe from read files.

--- gruntfile.coffee
+++ gruntfile.coffee
@@ -34,6 +34,14 @@ module.exports = (grunt) ->
             ]
           }
         ]
+      fromFiles:
+        options:
+          destDir: 'samples/dist/fromFiles'
+        src: [
+          'samples/src/build.json',
+          'samples/src/build.yml',
+          #'samples/src/build.txt'
+        ]
 
   grunt.loadTasks 'tasks'
 
--- tasks/domtage.js
+++ tasks/domtage.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var path = require('path');
 var Domtage = require('domtage');
 
 module.exports = function (grunt) {
@@ -7,7 +8,29 @@ module.exports = function (grunt) {
   grunt.registerMultiTask('domtage', 'generate html-files with domtage', function () {
 
     var opts = this.options();
-    new Domtage(opts, this.data.recipe).generate();
+    var recipe = this.data.recipe;
+
+    if(!recipe) {
+      if(this.filesSrc.length === 0) {
+        grunt.log.errorlns('Could not find any recipe files.');
+        return;
+      }
+
+      recipe = this.filesSrc.map(function(src){
+        var ext = path.extname(src);
+        if(ext === '.json') {
+          return grunt.file.readJSON(src);
+        }
+        else if(ext === '.yml' || ext === '.yaml') {
+          return grunt.file.readYAML(src);
+        }
+        else {
+          grunt.fail.warn(src + 'is could not use format. recipe file must JSON or YAML.');
+        }
+      });
+    }
+
+    new Domtage(opts, recipe).generate();
     grunt.log.ok('all recipes complete.');
   });
 };
